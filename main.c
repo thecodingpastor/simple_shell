468d4c5abac1b99e0de5972a2a41352f40e692b2
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-09-11 13:09:34 +0000

    still on shell

--- main.c
+++ main.c
@@ -12,8 +12,12 @@ int main(int argc, char **argv)
 
 	ssize_t numCharsRead;
 	char *prompt = "$ ";
-	char *linePointer = NULL, *linePointerCP = NULL;
+	char *linePointer = NULL, *ms_linePointerCP = NULL;
 	size_t lineSize = 0;
+	char *separator = " \n";
+	char *token;
+	int num_of_tokens = 0;
+	int count;
 
 	while (true)
 	{
@@ -25,7 +29,36 @@ int main(int argc, char **argv)
 			printf("Exiting...\n");
 			return (-1);
 		}
-		printf("%s\n", linePointer), free(linePointer);
+		ms_linePointerCP = malloc(sizeof(char) * numCharsRead);
+				if (ms_linePointerCP == NULL)
+			{
+				perror("memory allocation error");
+				return (-1);
+			}
+				strcpy(ms_linePointerCP, linePointer);
+				token = strtok(linePointer, separator);
+				while (token != NULL)
+				{
+				num_of_tokens++;
+				token = strtok(NULL, separator);
+				}
+				num_of_tokens++;
+				
+				argv = malloc(sizeof(char*) * num_of_tokens);
+				token = strtok(ms_linePointerCP, separator);
+				for(count = 0; token != NULL; count++)
+				{
+				argv[count] = malloc(sizeof(char) * strlen(token));
+				strcpy(argv[count], token);
+				token = strtok(NULL, separator);
+				}
+				argv[count] = NULL;
+
+		printf("%s\n", linePointer);
+
+		free(linePointer);
+		free(argv);
+		free(ms_linePointerCP);
 	}
 	return (0);
 }
