e66b5b9916f9b54441506f974ee8c1b3cf271744
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-09-11 12:02:15 +0000

    Working on exec

---
+++ exec.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void ms_exec(char **argv)
+{
+
+}
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+void ms_exec(char **argv);
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
