5f5d8f469cc08b8d41c29c0c8b1884a2a4b0975f
roteque <olusomogoye@gmail.com>
2023-09-20 15:00:46 -0700

    Split Token Function

--- utils.c
+++ utils.c
@@ -28,3 +28,26 @@ void handleError(char *msg, bool exitApp)
 	if (exitApp)
 		exit(2);
 }
+
+/**
+ * splitToTokens - splits arguments into tokens
+ * @ms_cmd: pointer to the full commands string passed
+ * @argv: arg vectors
+ * Return: number of tokens.
+ */
+
+size_t splitToTokens(char *ms_cmd, char *argv[])
+{
+	char *ms_delim = " ";
+	size_t ms_numTokens = 0;
+	char *ms_token = strtok(ms_cmd, ms_delim);
+
+	while (ms_token)
+	{
+		argv[ms_numTokens] = ms_token;
+		ms_token = strtok(NULL, ms_delim);
+		ms_numTokens++;
+	}
+	argv[ms_numTokens] = NULL;
+	return (ms_numTokens);
+}
