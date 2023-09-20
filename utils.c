9f25f6ea5888ae6f450f333478e80d4ab9234388
roteque <olusomogoye@gmail.com>
2023-09-20 14:54:13 -0700

    print string added again

--- utils.c
+++ utils.c
@@ -6,3 +6,13 @@ void prompt(void)
 {
 	printString("shelly_$ ");
 }
+
+/**
+ * printString - prints string
+ * @str: string to be printed
+ * Return: number of strings printed
+ */
+int printString(char *str)
+{
+	return (write(STDOUT_FILENO, str, _strlen(str)));
+}
