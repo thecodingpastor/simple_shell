22ecba5f27cbc5c1956d258db33788614670b9e8
roteque <olusomogoye@gmail.com>
2023-09-20 08:20:26 -0700

    String to character fnt added

--- strFnc.c
+++ strFnc.c
@@ -34,3 +34,19 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
+int _atoi(char *str)
+{
+	int result = 0, sign = 1, i = 0;
+
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+	i++;
+	if (str[i] == '-')
+	sign = -1, i++;
+	else if (str[i] == '+')
+	i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	result = result * 10 + (str[i] - '0'), i++;
+
+	return (result * sign);
+}
