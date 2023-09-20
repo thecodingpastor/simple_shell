51a985aea735983fe0f6022e863ca16c49d190a1
roteque <olusomogoye@gmail.com>
2023-09-20 14:56:48 -0700

    Handle Error Function

--- utils.c
+++ utils.c
@@ -16,3 +16,15 @@ int printString(char *str)
 {
 	return (write(STDOUT_FILENO, str, _strlen(str)));
 }
+
+/**
+ * handleError - prints errors
+ * @msg: message to be printed
+ * @exitApp: 1 or 0 to indicate exit after error
+ */
+void handleError(char *msg, bool exitApp)
+{
+	printString(msg);
+	if (exitApp)
+		exit(2);
+}
