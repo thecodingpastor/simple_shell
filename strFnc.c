31181e28dc7cf2bbcf93f178629e1e6c8985db35
roteque <olusomogoye@gmail.com>
2023-09-20 08:30:38 -0700

    String copy fnt added

--- strFnc.c
+++ strFnc.c
@@ -78,3 +78,25 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: destination
+ * @src: source
+ * Return: Returns a char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do
+	{
+	i++;
+	dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
